f1152f3d8c6fe4ca1a80fac1f7c9073a3d357170
natalia <nataliav@gmail.com>
2020-12-03 09:06:36 -0500

    formatting

--- 2020/src/dev/erikiva/aoc2020/Day3.kt
+++ 2020/src/dev/erikiva/aoc2020/Day3.kt
@@ -1,10 +1,12 @@
 package dev.erikiva.aoc2020
 
-class Day3 (){
-    fun findTrees(forest: List<String>): Int{
-        return findTreesForSlope(forest, arrayOf(3,1))
+class Day3 {
+
+    fun findTrees(forest: List<String>): Int {
+        return findTreesForSlope(forest, arrayOf(3, 1))
     }
-    fun findTrees2(forest: List<String>, slopes: List<Array<Int>>): Long{
+
+    fun findTrees2(forest: List<String>, slopes: List<Array<Int>>): Long {
         var result = 1L
         for (slope in slopes) {
             result *= findTreesForSlope(forest, slope)
@@ -16,7 +18,7 @@ class Day3 (){
         var trees = 0
         var pos = 0
         for (y in forest.indices step slope[1]) {
-            if (forest[y][pos] == '#'){
+            if (forest[y][pos] == '#') {
                 trees++
             }
             pos = getNextPos(pos, slope[0], forest[y].length)
@@ -25,10 +27,10 @@ class Day3 (){
         return trees
     }
 
-    private fun getNextPos(x: Int, step:Int, rowSize:Int):Int{
-        var next = x + step
-        if (next >= rowSize){
-            next -= (rowSize)
+    private fun getNextPos(x: Int, step: Int, rowSize: Int): Int {
+        val next = x + step
+        if (next >= rowSize) {
+            return next - rowSize
         }
         return next
     }
