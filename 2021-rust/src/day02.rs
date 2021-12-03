e450f48555f8bd413cb90619354b0000c22fdc6b
natalia <nataliav@gmail.com>
2021-12-02 23:07:34 -0500

    Refactor day2

--- 2021-rust/src/day02.rs
+++ 2021-rust/src/day02.rs
@@ -1,4 +1,4 @@
-type Move = (String, i32);
+type Move = (char, i32);
 type Position = (i32, i32);
 
 #[aoc_generator(day2)]
@@ -8,25 +8,26 @@ pub fn input_generator(input: &str) -> Vec<Move> {
         .map(|l| {
           let mut instruction = l.trim().split(' ');
           (
-              instruction.next().unwrap().to_string(),
+              instruction.next().unwrap().chars().next().unwrap(),
               instruction.next().unwrap().parse().unwrap(),
           )
   }).collect()
 }
 
+
 #[aoc(day2, part1)]
 pub fn solve_part1(input: &[Move]) -> i32 {
-  let mut  position : Position = (0, 0);
-  for instr in input {
-    if instr.0.as_str() == "forward" {
-      position.1 += instr.1
-    } else if instr.0.as_str() == "up" {
-      position.0 -= instr.1
-    } else {
-      position.0 += instr.1
+  let pos = input.iter().fold((0,0), |mut position, instr| {
+    match instr {
+      ('f', n) => position.1 += n,
+      ('u', n) => position.0 -= n,
+      ('d', n) => position.0 += n,
+      _ => ()
     }
-  }
-  return position.0 * position.1;
+    position
+  });
+
+  return pos.0 * pos.1;
 }
 
 #[aoc(day2, part2)]
@@ -34,13 +35,12 @@ pub fn solve_part2(input: &[Move]) -> i32 {
   let mut  position : Position = (0, 0);
   let mut aim: i32 = 0;
   for instr in input {
-    if instr.0.as_str() == "forward" {
-      position.0 += instr.1;
-      position.1 += aim * instr.1;
-    } else if instr.0.as_str() == "up" {
-      aim -= instr.1;
-    } else {
-      aim += instr.1;
+    match instr {
+      ('f', n) => {position.0 += n;
+        position.1 += aim * instr.1},
+      ('u', n) => aim -= n,
+      ('d', n) => aim += n,
+      _ => ()
     }
   }
   return position.0 * position.1;
